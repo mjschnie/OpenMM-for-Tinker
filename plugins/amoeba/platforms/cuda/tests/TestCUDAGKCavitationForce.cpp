8aad23e9d01b657e74c1228c79c8ebef0fe87c83
Andrew <andthiel@uiowa.edu>
2020-04-21 14:55:19 -0500

    Added CUDA test for GKCavitation Force

--- plugins/amoeba/platforms/cuda/tests/TestCUDAGKCavitationForce.cpp
+++ plugins/amoeba/platforms/cuda/tests/TestCUDAGKCavitationForce.cpp
@@ -1,25 +1,28 @@
-//
-// Created by Thiel, Andrew on 2019-06-07.
-//
+/* -------------------------------------------------------------------------- *
+ *                              OpenMM-GKCavitation                                 *
+ * -------------------------------------------------------------------------- */
+
+/**
+ * This tests the CUDA implementation of GKNPForce.
+ */
+
 #define _USE_MATH_DEFINES // Needed to get M_PI
-#include "OpenMM.h"
+#include "openmm/AmoebaGKCavitationForce.h"
+#include "openmm/internal/AssertionUtilities.h"
 #include "openmm/Context.h"
 #include "openmm/Platform.h"
 #include "openmm/System.h"
 #include "openmm/VerletIntegrator.h"
-#include "openmm/AmoebaGKCavitationForce.h"
-#include <iosfwd>
-#include <map>
-#include <string>
-#include <vector>
-#include <iostream>
-#include <iomanip>
+#include "openmm/NonbondedForce.h"
 #include <cmath>
+#include <iostream>
+#include <vector>
+#include <iomanip>
 
 using namespace OpenMM;
 using namespace std;
 
-extern "C" OPENMM_EXPORT void registerAmoebaReferenceKernelFactories();
+extern "C" OPENMM_EXPORT void registerAmoebaCudaKernelFactories();
 
 static struct MyAtomInfo {
     const char* pdb;
@@ -30,8 +33,8 @@ static struct MyAtomInfo {
 } atoms[] = {
 
         // pdb   mass vdwRad vdwVol gamma isHydrogen charge,  initPos
-        {" C ", 12.00, 1.91, 28.28,  .16,  false, -0.18,  0.76506600,    0.00000200,   -0.00000100},
-        {" C ", 12.00, 1.91, 28.28,  .16,  false, -0.18, -0.76506500,   -0.00000200,    0.00000100},
+        {" C ", 12.00, 1.91, 28.28,  0.16,  false, -0.18,  0.76506600,    0.00000200,   -0.00000100},
+        {" C ", 12.00, 1.91, 28.28,  0.16,  false, -0.18, -0.76506500,   -0.00000200,    0.00000100},
         {" H ",  1.00, 1.48, 12.77,  0,     true,   0.06, -1.16573300,    0.67232500,    0.77710400},
         {" H ",  1.00, 1.48, 12.77,  0,     true,   0.06, -1.16574800,    0.33683200,   -0.97079400},
         {" H ",  1.00, 1.48, 12.77,  0,     true,   0.06, -1.16572400,   -1.00915800,    0.19369400},
@@ -41,11 +44,13 @@ static struct MyAtomInfo {
         {""} // end of list
 };
 
-void testEnergy() {
+void testForce() {
+
+    bool veryverbose = false;
 
     System system;
     NonbondedForce *nb = new NonbondedForce();
-    AmoebaGKCavitationForce *force = new AmoebaGKCavitationForce();
+    AmoebaGKCavitationForce* force = new AmoebaGKCavitationForce();
     force->setNonbondedMethod(AmoebaGKCavitationForce::NoCutoff);//NoCutoff also accepted
     force->setCutoffDistance(1.0);
     system.addForce(nb);
@@ -86,7 +91,7 @@ void testEnergy() {
     }
 
     VerletIntegrator integ(1.0);
-    Platform& platform = Platform::getPlatformByName("Reference");
+    Platform& platform = Platform::getPlatformByName("CUDA");
 
     Context context(system, integ, platform);
     context.setPositions(positions);
@@ -101,16 +106,31 @@ void testEnergy() {
     cout << std::setw(25) << std::left << "Surface Area Energy: " << surfaceAreaEnergy << " (kcal/mol)" << endl << endl;
 
     cout << "Forces: " << endl;
-    for(int i = 0; i < numParticles; i++){
-        cout << "FW: " << i << " " << state.getForces()[i][0] << " " << state.getForces()[i][1] << " "  << state.getForces()[i][2] << " "<< endl;
+    for(int i = 0; i < numParticles; i++) {
+        cout << "FW: " << i << " " << state.getForces()[i][0] << " " << state.getForces()[i][1] << " "
+             << state.getForces()[i][2] << " " << endl;
     }
+
+    // validate force by moving an atom
+#ifdef NOTNOW
+    double offset = 2.e-3;
+    int pmove = 121;
+    int direction = 1;
+    positions[pmove][direction] += offset;
+    context.setPositions(positions);
+    double energy2 = context.getState(State::Energy).getPotentialEnergy();
+    double de = -state.getForces()[pmove][direction]*offset;
+    std::cout << "Energy: " <<  energy2  << std::endl;
+    std::cout << "Energy Change: " <<  energy2 - energy1  << std::endl;
+    std::cout << "Energy Change from Gradient: " <<  de  << std::endl;
+#endif
 
 }
 
-int main(){
+int main() {
     try {
-        registerAmoebaReferenceKernelFactories();
-        testEnergy();
+        registerAmoebaCudaKernelFactories();
+        testForce();
     }
     catch(const std::exception& e) {
         std::cout << "exception: " << e.what() << std::endl;
@@ -118,3 +138,4 @@ int main(){
     }
     return 0;
 }
+
