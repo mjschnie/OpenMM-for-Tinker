3ab1722ca57f635f470994acab737ef4650d36fa
Michael J. Schnieders <michael-schnieders@uiowa.edu>
2020-05-08 16:01:44 -0500

    Prevent Born radii from being smaller than their base radii

--- plugins/amoeba/platforms/reference/src/SimTKReference/AmoebaReferenceGeneralizedKirkwoodForce.cpp
+++ plugins/amoeba/platforms/reference/src/SimTKReference/AmoebaReferenceGeneralizedKirkwoodForce.cpp
@@ -28,13 +28,13 @@ using std::vector;
 using namespace OpenMM;
 
 AmoebaReferenceGeneralizedKirkwoodForce::AmoebaReferenceGeneralizedKirkwoodForce() : _numParticles(0),
-                                                                                      _includeCavityTerm(1),
-                                                                                      _directPolarization(0),
-                                                                                      _soluteDielectric(1.0),
-                                                                                      _solventDielectric(78.3),
-                                                                                      _dielectricOffset(0.009),
-                                                                                      _probeRadius(0.14),
-                                                                                      _surfaceAreaFactor(0.0054) {
+                                                                                     _includeCavityTerm(1),
+                                                                                     _directPolarization(0),
+                                                                                     _soluteDielectric(1.0),
+                                                                                     _solventDielectric(78.3),
+                                                                                     _dielectricOffset(0.009),
+                                                                                     _probeRadius(0.14),
+                                                                                     _surfaceAreaFactor(0.0054) {
 
 }
 
@@ -63,7 +63,7 @@ int AmoebaReferenceGeneralizedKirkwoodForce::getDirectPolarization() const {
 }
 
 void AmoebaReferenceGeneralizedKirkwoodForce::setSoluteDielectric(double soluteDielectric) {
-    _soluteDielectric  = soluteDielectric;
+    _soluteDielectric = soluteDielectric;
 }
 
 double AmoebaReferenceGeneralizedKirkwoodForce::getSoluteDielectric() const {
@@ -71,7 +71,7 @@ double AmoebaReferenceGeneralizedKirkwoodForce::getSoluteDielectric() const {
 }
 
 void AmoebaReferenceGeneralizedKirkwoodForce::setSolventDielectric(double solventDielectric) {
-    _solventDielectric  = solventDielectric;
+    _solventDielectric = solventDielectric;
 }
 
 double AmoebaReferenceGeneralizedKirkwoodForce::getSolventDielectric() const {
@@ -79,7 +79,7 @@ double AmoebaReferenceGeneralizedKirkwoodForce::getSolventDielectric() const {
 }
 
 void AmoebaReferenceGeneralizedKirkwoodForce::setDielectricOffset(double dielectricOffset) {
-    _dielectricOffset  = dielectricOffset;
+    _dielectricOffset = dielectricOffset;
 }
 
 double AmoebaReferenceGeneralizedKirkwoodForce::getDielectricOffset() const {
@@ -87,7 +87,7 @@ double AmoebaReferenceGeneralizedKirkwoodForce::getDielectricOffset() const {
 }
 
 void AmoebaReferenceGeneralizedKirkwoodForce::setProbeRadius(double probeRadius) {
-    _probeRadius  = probeRadius;
+    _probeRadius = probeRadius;
 }
 
 double AmoebaReferenceGeneralizedKirkwoodForce::getProbeRadius() const {
@@ -95,44 +95,44 @@ double AmoebaReferenceGeneralizedKirkwoodForce::getProbeRadius() const {
 }
 
 void AmoebaReferenceGeneralizedKirkwoodForce::setSurfaceAreaFactor(double surfaceAreaFactor) {
-    _surfaceAreaFactor  = surfaceAreaFactor;
+    _surfaceAreaFactor = surfaceAreaFactor;
 }
 
 double AmoebaReferenceGeneralizedKirkwoodForce::getSurfaceAreaFactor() const {
     return _surfaceAreaFactor;
 }
 
-void AmoebaReferenceGeneralizedKirkwoodForce::setAtomicRadii(const vector<double>& atomicRadii) {
+void AmoebaReferenceGeneralizedKirkwoodForce::setAtomicRadii(const vector<double> &atomicRadii) {
     _atomicRadii.resize(atomicRadii.size());
     copy(atomicRadii.begin(), atomicRadii.end(), _atomicRadii.begin());
 }
 
-void AmoebaReferenceGeneralizedKirkwoodForce::getAtomicRadii(vector<double>& atomicRadii) const {
+void AmoebaReferenceGeneralizedKirkwoodForce::getAtomicRadii(vector<double> &atomicRadii) const {
     atomicRadii.resize(_atomicRadii.size());
     copy(_atomicRadii.begin(), _atomicRadii.end(), atomicRadii.begin());
 }
 
-void AmoebaReferenceGeneralizedKirkwoodForce::setScaleFactors(const vector<double>& scaleFactors) {
+void AmoebaReferenceGeneralizedKirkwoodForce::setScaleFactors(const vector<double> &scaleFactors) {
     _scaleFactors.resize(scaleFactors.size());
     copy(scaleFactors.begin(), scaleFactors.end(), _scaleFactors.begin());
 }
 
-void AmoebaReferenceGeneralizedKirkwoodForce::getScaleFactors(vector<double>& scaleFactors) const {
+void AmoebaReferenceGeneralizedKirkwoodForce::getScaleFactors(vector<double> &scaleFactors) const {
     scaleFactors.resize(_scaleFactors.size());
     copy(_scaleFactors.begin(), _scaleFactors.end(), scaleFactors.begin());
 }
 
-void AmoebaReferenceGeneralizedKirkwoodForce::setCharges(const vector<double>& charges) {
+void AmoebaReferenceGeneralizedKirkwoodForce::setCharges(const vector<double> &charges) {
     _charges.resize(charges.size());
     copy(charges.begin(), charges.end(), _charges.begin());
 }
 
-void AmoebaReferenceGeneralizedKirkwoodForce::getGrycukBornRadii(vector<double>& bornRadii) const {
+void AmoebaReferenceGeneralizedKirkwoodForce::getGrycukBornRadii(vector<double> &bornRadii) const {
     bornRadii.resize(_bornRadii.size());
     copy(_bornRadii.begin(), _bornRadii.end(), bornRadii.begin());
 }
 
-void AmoebaReferenceGeneralizedKirkwoodForce::calculateGrycukBornRadii(const vector<Vec3>& particlePositions) {
+void AmoebaReferenceGeneralizedKirkwoodForce::calculateGrycukBornRadii(const vector<Vec3> &particlePositions) {
 
     const double bigRadius = 1000.0;
 
@@ -147,56 +147,65 @@ void AmoebaReferenceGeneralizedKirkwoodForce::calculateGrycukBornRadii(const vec
         double bornSum = 0.0;
         for (unsigned int jj = 0; jj < _numParticles; jj++) {
 
-            if (ii == jj || _atomicRadii[jj] < 0.0)continue;
-          
-            double xr       = particlePositions[jj][0] - particlePositions[ii][0];
-            double yr       = particlePositions[jj][1] - particlePositions[ii][1];
-            double zr       = particlePositions[jj][2] - particlePositions[ii][2];
+            if (ii == jj || _atomicRadii[jj] <= 0.0) continue;
 
-            double r2       = xr*xr + yr*yr + zr*zr;
-            double r        = sqrt(r2);
+            double xr = particlePositions[jj][0] - particlePositions[ii][0];
+            double yr = particlePositions[jj][1] - particlePositions[ii][1];
+            double zr = particlePositions[jj][2] - particlePositions[ii][2];
 
-            double sk       = _atomicRadii[jj]*_scaleFactors[jj];
+            double r2 = xr * xr + yr * yr + zr * zr;
+            double r = sqrt(r2);
+
+            double sk = _atomicRadii[jj] * _scaleFactors[jj];
 
             // If atom ii engulfs the descreening atom, then continue.
             if (_atomicRadii[ii] > r + sk) continue;
 
-            double sk2      = sk*sk;
+            double sk2 = sk * sk;
 
             if ((_atomicRadii[ii] + r) < sk) {
-                double lik       = _atomicRadii[ii];
-                double uik       = sk - r;  
-                double lik3      = lik*lik*lik;
-                double uik3      = uik*uik*uik;
-                bornSum             -= (1.0/uik3 - 1.0/lik3);
-            }   
-        
-            double uik = r + sk; 
+                double lik = _atomicRadii[ii];
+                double uik = sk - r;
+                double lik3 = lik * lik * lik;
+                double uik3 = uik * uik * uik;
+                bornSum -= (1.0 / uik3 - 1.0 / lik3);
+            }
+
+            double uik = r + sk;
             double lik;
             if ((_atomicRadii[ii] + r) < sk) {
-                lik = sk - r;  
+                lik = sk - r;
             } else if (r < (_atomicRadii[ii] + sk)) {
                 lik = _atomicRadii[ii];
             } else {
-                lik = r - sk; 
-            }   
-        
-            double l2          = lik*lik; 
-            double l4          = l2*l2;
-            double lr          = lik*r;
-            double l4r         = l4*r;
-        
-            double u2          = uik*uik;
-            double u4          = u2*u2;
-            double ur          = uik*r;
-            double u4r         = u4*r;
-        
-            double term        = (3.0*(r2-sk2) + 6.0*u2 - 8.0*ur)/u4r - (3.0*(r2-sk2) + 6.0*l2 - 8.0*lr)/l4r;
-            bornSum           += term/16.0;
-        
+                lik = r - sk;
+            }
+
+            double l2 = lik * lik;
+            double l4 = l2 * l2;
+            double lr = lik * r;
+            double l4r = l4 * r;
+
+            double u2 = uik * uik;
+            double u4 = u2 * u2;
+            double ur = uik * r;
+            double u4r = u4 * r;
+
+            double term =
+                    (3.0 * (r2 - sk2) + 6.0 * u2 - 8.0 * ur) / u4r
+                    - (3.0 * (r2 - sk2) + 6.0 * l2 - 8.0 * lr) / l4r;
+            bornSum += term / 16.0;
+
         }
-        bornSum        = 1.0/(_atomicRadii[ii]*_atomicRadii[ii]*_atomicRadii[ii]) - bornSum;
-        _bornRadii[ii] = (bornSum <= 0.0) ? bigRadius : pow(bornSum, -1.0/3.0);
+
+        bornSum = 1.0 / (_atomicRadii[ii] * _atomicRadii[ii] * _atomicRadii[ii]) - bornSum;
+
+        _bornRadii[ii] = (bornSum <= 0.0) ? bigRadius : pow(bornSum, -1.0 / 3.0);
+
+        // Born radius should be at least as large as its base radius.
+        if (_bornRadii[ii] < _atomicRadii[ii]) {
+            _bornRadii[ii] = _atomicRadii[ii];
+        }
     }
 
     return;
